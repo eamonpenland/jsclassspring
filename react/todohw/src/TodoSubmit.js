b2d3cf099fe64bf309c4df335cf66f0670419100
eamonpenland <eamon.penland@gmail.com>
2017-05-16 19:11:34 -0400

    auth protected

--- react/todohw/src/TodoSubmit.js
+++ react/todohw/src/TodoSubmit.js
@@ -1,22 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoSubmit extends Component {
-  _handleButtonPress = () => {
-    const {editingTodo, updateTodo, addNewTodo, newTodoInput} = this.props;
-
-    editingTodo ? updateTodo() : addNewTodo(newTodoInput)
-  }
-  render() {
-    const { newTodoInput, changeTodoVal } = this.props;
-
-    return (
-      <div>
-        <input value={newTodoInput} onChange={(e) => changeTodoVal(e.target.value)}/>
-        <button onClick={this._handleButtonPress}>Submit</button>
-      </div>
-    )
-  }
-}
-
+const TodoSubmit = ({editingTodo, updateTodo, addNewTodo, newTodoInput, changeTodoVal}) => (
+  <div>
+    <input value={newTodoInput} onChange={(e) => changeTodoVal(e.target.value)}/>
+    <button onClick={() => editingTodo ? updateTodo() : addNewTodo(newTodoInput)}>Submit</button>
+  </div>
+)
 
 export default TodoSubmit;
--- react/todohw/src/todoslist.js
+++ react/todohw/src/todoslist.js
@@ -1,35 +1,60 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodosList extends Component {
-  _handleTodos = (todos, activeFilter, changeStatus, editTodo) => {
-    return todos.filter(todo => {
-      return todo.status === activeFilter;
-    }).map((todo, i) => {
-      const editedStyle = todo.edited ? 'orange' : 'black';
+const TodosList = ({ todos, activeFilter, changeStatus, editTodo }) => {
+  return (
+    <div>
+      {
+        todos.filter(todo => {
+          return todo.status === activeFilter;
+        }).map((todo, i) => {
+          const editedStyle = todo.edited ? 'orange' : 'black';
 
-      return (
-        <div key={i} style={{color: editedStyle}}>
-          <li
-            onClick={() => changeStatus(todo.id)}
-            >
-              {todo.desc}
-          </li>
-          <span onClick={() => editTodo(todo)}>Edit</span>
-        </div>
-      )
-    })
-  }
-
-  render() {
-    const { todos, activeFilter, changeStatus, editTodo } = this.props;
-    const renderTodos = this._handleTodos(todos, activeFilter, changeStatus, editTodo);
-
-    return (
-      <div>
-        {renderTodos}
-      </div>
-    )
-  }
+          return (
+            <div key={i} style={{color: editedStyle}}>
+              <li
+                onClick={() => changeStatus(todo.id)}
+                >
+                  {todo.desc}
+              </li>
+              <span onClick={() => editTodo(todo)}>Edit</span>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
+
+// class TodosList extends Component {
+//   _handleTodos = (todos, activeFilter, changeStatus, editTodo) => {
+    // return todos.filter(todo => {
+    //   return todo.status === activeFilter;
+    // }).map((todo, i) => {
+    //   const editedStyle = todo.edited ? 'orange' : 'black';
+    //
+    //   return (
+    //     <div key={i} style={{color: editedStyle}}>
+    //       <li
+    //         onClick={() => changeStatus(todo.id)}
+    //         >
+    //           {todo.desc}
+    //       </li>
+    //       <span onClick={() => editTodo(todo)}>Edit</span>
+    //     </div>
+    //   )
+    // })
+//   }
+//
+//   render() {
+//     const { todos, activeFilter, changeStatus, editTodo } = this.props;
+//     const renderTodos = this._handleTodos(todos, activeFilter, changeStatus, editTodo);
+//
+//     return (
+//       <div>
+//         {renderTodos}
+//       </div>
+//     )
+//   }
+// }
 
 export default TodosList
